e70572d7354f12698c3734054fa6d29be384bbee
steven754 <16986754@qq.com>
2021-01-02 14:26:40 +0800

    os

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("hello world")
+
+import os
+
+lst = os.listdir(os.getcwd())
+
+for c in lst:
+    if os.path.isfile(c) and c.endswith('.py'): #and c.find("AllTest") == -1:  # 去掉AllTest.py文件
+        print(c)
+        os.system(os.path.join(os.getcwd(), c))
