feb08e0082b8b4b84a858a64e09730d8387b359f
steven754 <16986754@qq.com>
2021-01-02 14:38:29 +0800

    path

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "开始测试......"
-python /var/jrnkins_home/git/myjenkins/python1.py
+python /var/jrnkins_home/git/myjenkins_test/python1.py
 wait
-python /var/jrnkins_home/git/myjenkins/python2.py
+python /var/jrnkins_home/git/myjenkins_test/python2.py
 wait
-python /var/jrnkins_home/git/myjenkins/python3.py
+python /var/jrnkins_home/git/myjenkins_test/python3.py
 
 echo "结束测试......"
