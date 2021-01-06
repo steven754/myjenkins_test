25b91d21ce8e1b373019e4b1d94e4e4402c791c8
steven754 <16986754@qq.com>
2021-01-06 16:12:27 +0800

    modify

--- hello.py
+++ hello.py
@@ -2,9 +2,4 @@ print("hello world")
 #econding="utf-8"
 import os
 
-lst = os.listdir(os.getcwd())
-
-for c in lst:
-    if os.path.isfile(c) and c.endswith('.py'): #and c.find("AllTest") == -1:  # 去掉AllTest.py文件
-        print(c)
-        #os.system(os.path.join(os.getcwd(), c))
+print("hello,world")
